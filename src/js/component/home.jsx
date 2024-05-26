326c79345b273ff445b2a9862f191b520649d552
Daniel Pena <163460072+daperk@users.noreply.github.com>
2024-05-26 00:29:53 +0000

    to do done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function TodoItem({ label, isDone, onToggle, onDelete }) {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" checked={isDone} onChange={onToggle} />
+      <span className="todo-text">{label}</span>
+      <button className="btn btn-danger" onClick={onDelete}>Delete</button>
+    </div>
+  );
+}
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function Home() {
+  const [todos, setTodos] = useState([
+    { label: "Go to the supermarket.", isDone: false },
+    { label: "Buy cereal.", isDone: false },
+    { label: "Make oil change to the car before 5/30.", isDone: false },
+    { label: "Go home and clean the house before 11:00 PM", isDone: false },
+  ]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { label: newTodo, isDone: false }]);
+      setNewTodo("");
+    }
+  };
+
+  const toggleTodo = (index) => {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, isDone: !todo.isDone } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Todo List</h1>
+      <input
+        className="form-control"
+        type="text"
+        placeholder="What do you want to get done today?"
+        value={newTodo}
+        onChange={handleInputChange}
+      />
+      <button className="btn btn-primary" onClick={addTodo}>Add</button>
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={index}
+          label={todo.label}
+          isDone={todo.isDone}
+          onToggle={() => toggleTodo(index)}
+          onDelete={() => deleteTodo(index)}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default Home;
